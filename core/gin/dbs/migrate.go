718e370cca281342c4327c7699a4959358337979
AAAxianyu <24061823@hdu.edu.cn>
2025-03-19 00:10:23 +0800

    feat:新增故事推送功能

--- core/gin/dbs/migrate.go
+++ core/gin/dbs/migrate.go
@@ -3,6 +3,7 @@ package dbs
 import (
 	"Programming-Demo/internal/app/File/file_entity"
 	"Programming-Demo/internal/app/ai/ai_entity"
+	"Programming-Demo/internal/app/story/story_entity"
 	"Programming-Demo/internal/app/template/template_entity"
 	"Programming-Demo/internal/app/user/user_entity"
 	"gorm.io/gorm"
@@ -15,6 +16,7 @@ func AutoMigrate(db *gorm.DB) error {
 		&file_entity.File{},
 		&ai_entity.ChatHistory{},
 		&template_entity.LegalTemplate{},
+		&story_entity.Story{},
 	)
 	return err
 }
