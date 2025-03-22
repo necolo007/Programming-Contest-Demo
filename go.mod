c2ea2243933e57d73a17afc08059f05a6ba90792
AAAxianyu <24061823@hdu.edu.cn>
2025-03-22 16:58:52 +0800

    feat:修正注册登录问题，优化推送实现和搜索功能实现

--- go.mod
+++ go.mod
@@ -56,6 +56,7 @@ require (
 	github.com/opentracing/opentracing-go v1.2.1-0.20220228012449-10b1cf09e00b // indirect
 	github.com/pelletier/go-toml/v2 v2.2.2 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
+	github.com/robfig/cron/v3 v3.0.1 // indirect
 	github.com/rogpeppe/go-internal v1.9.0 // indirect
 	github.com/sagikazarmark/locafero v0.4.0 // indirect
 	github.com/sagikazarmark/slog-shim v0.1.0 // indirect
--- internal/app/story/story_handler/story.go
+++ internal/app/story/story_handler/story.go
@@ -4,10 +4,8 @@ import (
 	"Programming-Demo/core/gin/dbs"
 	"Programming-Demo/core/libx"
 	"Programming-Demo/internal/app/story/story_entity"
-	"Programming-Demo/internal/app/user/user_entity"
 	"math/rand"
 	"net/http"
-	"time"
 
 	"github.com/gin-gonic/gin"
 )
@@ -23,28 +21,8 @@ func GetRandomStory(c *gin.Context) {
 		return
 	}
 
-	// 查询用户最后一次接收推送的时间
-	var lastPushTime time.Time
-	if err := dbs.DB.Model(&user_entity.User{}).Where("id = ?", uid).Select("last_push_time").Scan(&lastPushTime).Error; err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{
-			"code":    500,
-			"message": "获取用户信息失败",
-			"error":   err.Error(),
-		})
-		return
-	}
-
-	// 检查时间间隔是否达到2天
-	if time.Since(lastPushTime).Hours() < 48 {
-		c.JSON(http.StatusOK, gin.H{
-			"code":    200,
-			"message": "未达到推送间隔时间",
-		})
-		return
-	}
-
-	var story []story_entity.Story
-	if err := dbs.DB.Find(&story).Error; err != nil {
+	var stories []story_entity.Story
+	if err := dbs.DB.Find(&stories).Error; err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{
 			"code":    500,
 			"message": "获取故事列表失败",
@@ -52,7 +30,7 @@ func GetRandomStory(c *gin.Context) {
 		})
 		return
 	}
-	if len(story) == 0 {
+	if len(stories) == 0 {
 		c.JSON(http.StatusOK, gin.H{
 			"code":    200,
 			"message": "无可用故事",
@@ -60,18 +38,8 @@ func GetRandomStory(c *gin.Context) {
 		return
 	}
 
-	randomIndex := rand.Intn(len(story))
-	selectedStory := story[randomIndex]
-
-	// 更新用户最后一次接收推送的时间
-	if err := dbs.DB.Model(&user_entity.User{}).Where("id = ?", uid).Update("last_push_time", time.Now()).Error; err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{
-			"code":    500,
-			"message": "更新用户信息失败",
-			"error":   err.Error(),
-		})
-		return
-	}
+	randomIndex := rand.Intn(len(stories))
+	selectedStory := stories[randomIndex]
 
 	c.JSON(http.StatusOK, gin.H{
 		"code":    200,
